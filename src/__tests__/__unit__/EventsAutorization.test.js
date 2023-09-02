baa1091ec1b816e1675963fc3513196e2213ef32
Tryd0g0lik <work80@mail.ru>
2023-09-02 07:44:54 +0700

    temporary pouse for src\__tests__\__unit__\zero.test.js

--- src/__tests__/__unit__/EventsAutorization.test.js
+++ src/__tests__/__unit__/EventsAutorization.test.js
@@ -1,14 +1,16 @@
 const { handlers } = require('../../app/ts/functions');
+const puppeteer = require('puppeteer');
 
 let events = {};
 let users = null;
 let div = null;
 let button;
 const respons = test.each([
-	{ a: 'zero test', b: 'Enter', expected: 'Ok' }
+	{ a: 'zero test', b: 'Tute', expected: 'Ok' }
 ]);
 
 jest.mock('node-fetch');
+
 afterEach(() => {
 	global.fetch = jest.fn(() => {
 		Promise.resolve(() => {
@@ -22,32 +24,51 @@ afterEach(() => {
 
 });
 respons('click', async ({ a, b, expected }) => {
+	let ev = null;
+	const browser = await puppeteer.launch({
+		headless: false,
+		slowMo: 200,
+		devtools: true,
+		pipe: true,
+	});
+	const page = await browser.newPage();
+	const inputValue = b;
+	// const pageLocalH = await page.goto('http://localhost:9000');
+	// await pageLocalH.waitForTimeout(300);
+	// await pageLocalH.waitForSelector('.login');
+	// const form = await pageLocalH.$('logi');
 
-	// document.body.innerHTML = `<div>
-	// <button type="submit" id="button" />
-	// </div>`;
 
-	// const button = document.getElementById("button");
-	const button = document.createElement('button');
-	button.innerHTML = 'Нажми';
-	button.type = 'submit';
-	button.classList.add('butt');
 
-	console.log('HTML: ', button.tagName);
-	document.insertAdjacentElement('afterend', button);
-	// document.body.innerHTML =
-	// 	'<div>' +
-	// 	'  <span id="username" />' +
-	// 	'  <button id="butt" />' +
-	// 	'</div>';
+	await page.goto('http://localhost:9000')
+	await page.evaluate(() => {
+		document.body.addEventListener('submit', e => {
+			e.preventDefault()
+			ev = e;
+			// ev['key'] = 'Enter';
+			// ev['target'] = 'submit'
+		});
+	});
+	await page.waitForSelector('#login');
+	const input = await page.$('#login');
+	await input.type(b);
+	const button = await page.$('#go');
+	await button.target();
+	await button.type('submit');
 
-	const domButton = document.querySelector('#butt');
-	// domButton.innerHTML = "<h1>Привет!</h1>";
-	console.log('HTML domButton: ', domButton);
-	const ev = domButton.click();
+	await button.click();
+
+	// console.log('RESP: ', resp);
+
+	await handlers.EventsAutorization(ev);
+	page.waiteForOptions(15000);
+	// const watchDog = page.waitForFunction('window.innerWidth < 100');
+	// await page.setViewport({ width: 50, height: 50 });
 	// const ev = new Event('click', { bubbles: true });
-	console.log('E: ', domButton.click());
-	expect(domButton.click()).toBe('button')
-
-});
+	// await watchDog;
+	// const resp = domButton.click()
+	debugger;
+	expect(resp).toBe('button')
+	await browser.close();
+}, 17000);
 
