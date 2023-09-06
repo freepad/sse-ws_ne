420379ef61a570921b0c79449bf661db612bdf3d
Tryd0g0lik <work80@mail.ru>
2023-09-06 10:44:56 +0700

    starting works from the beginning

--- src.v0/__tests__/setupTests.js
+++
@@ -1,6 +0,0 @@
-// import myFile from './__e2e__/zero.test.js';
-import eventsAutorization from './__unit__/EventsAutorization.test.js';
-module.exports = {
-	// myFile,
-	eventsAutorization
-}
--- src.v0/index.js
+++
@@ -1,3 +0,0 @@
-import './app/scss/style.css';
-import './app/scss/style.scss';
-import './app/ts/main.ts';
