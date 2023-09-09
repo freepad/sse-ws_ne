941df01583262088dfbc6c526459fda0abda3dc2
Tryd0g0lik <work80@mail.ru>
2023-09-09 09:07:38 +0700

    БАЗОВАЯ рабочая версия

--- src/app/serve.ts
+++ src/app/serve.ts
@@ -1,7 +1,10 @@
+
+
 const http = require('http'),
 	Koa = require('koa'),
 	json = require('koa-json'),
 	cors = require('@koa/cors'),
+
 	// Router = require('koa-router'),
 // websockify = require('koa-websocket'),
 // router = new Router(),
@@ -15,15 +18,32 @@ const http = require('http'),
 
 const server = http.createServer(app.callback);
 const webSocketServer = new WS.Server({ server });
+let newClient = {};
 webSocketServer.on('connection', (ws: any) => {
 	ws.on('message', (m: any) => {
-		console.log('SERVER_: ', JSON.parse(m));
-		webSocketServer.clients.forEach((client: any) => client.send(m));
+		console.log('SERVER_: ', typeof JSON.parse(JSON.parse(m)), JSON.parse(JSON.parse(m)));
+		const message = JSON.parse(JSON.parse(m));
+		const result = db.logins.find((elem: any) => elem['login'] === message['newLogin']);
+		console.log('RESULT: ', result);
+		if (result === undefined) {
+			newClient = { login: message['newLogin'], id: makeUniqueId(v4()) };
+			db.logins.push(newClient);
+			// webSocketServer.clients.forEach((client: any) => client.send(JSON.stringify(newClient)));
+
+
+			console.log('TYU: ', JSON.stringify(newClient))
+			const tyu = JSON.stringify(newClient);
+			ws.send(tyu);
+		};
 	});
+
+
+	// if (JSON.stringify(newClient) !== '{}') {
+	// }
 
 	ws.on("error", (e: any) => ws.send(e));
 
-	ws.send('Hi there, I am a WebSocket server');
+	// ws.send('Hi there, I am a WebSocket server');
 });
 server.listen(7070, () => console.log("Server started"));
 
--- src/app/ts/main.ts
+++ src/app/ts/main.ts
@@ -10,149 +10,83 @@ const { fun } = require('./functions');
 // 	alert("Соединение установлено.");
 // };
 
-//document.addEventListener('DOMContentLoaded', () => {
-/*------------------*/
-const wsserver = new WebSocket("ws://localhost:7070");
-console.log('Страница загрузилась');
-const body = document.getElementsByTagName('body') as HTMLCollectionOf<HTMLElement>;
-(body[0].querySelector('.chattalks > div:last-of-type') as HTMLElement).setAttribute('style', "display:none;")
-body[0].insertAdjacentHTML("afterbegin", fun.forms());
-
-const formIdentification = body[0].querySelector('.author') as HTMLFormElement;
-const input = body[0].querySelector('input') as HTMLInputElement;
-console.log("Получили форму для регистрации New Login!");
-// const result = fun.author();
-// console.log('Отправляем: ', result);
-
-// window.addEventListener('offline', (e: any) => {
-// 	console.warn("Note: User's browser id ofline now!");
-// });
-
-
-
-// formIdentification.
-let answ = {};
-let resultOfFormIdentification = '';
-formIdentification.addEventListener('keypress', async (e: any) => {
-	if (((e as KeyboardEvent).key === 'Enter')) {
-		e.preventDefault();
-		sendToServe(e) as object;
-
-	}
-
-});
-
-formIdentification.addEventListener('click', async (e: any) => {
-	if (((e as MouseEvent).target as HTMLButtonElement).type === 'submit') {
-		sendToServe(e) as object;
-
-	}
-});
-
-function sendToServe(e: any) {
-	e.preventDefault();
-
-	if (input.value.length < 1) { console.log('Длина INPUT = 0:', input.value.length); return }
-	console.log('SUBMIT'),
-		console.log('Прослушка -  получили событие Inpuut из формы New-Login');
-	resultOfFormIdentification = JSON.stringify(fun.author(e));
-	if (resultOfFormIdentification === '{"newLogin":""}') return {}
-	console.log('test 01: ', resultOfFormIdentification);
-	// debugger;
-	console.log('New-Login отправлено!');
-
-	// }
-
+document.addEventListener('DOMContentLoaded', () => {
+	/*------------------*/
 	const wsConnection = new WebSocket("ws://localhost:7070");
+	// const wsserver = new WebSocket("ws://localhost:7070");
+	console.log('Страница загрузилась');
+	const body = document.getElementsByTagName('body') as HTMLCollectionOf<HTMLElement>;
+	(body[0].querySelector('.chattalks > div:last-of-type') as HTMLElement).setAttribute('style', "display:none;")
+	body[0].insertAdjacentHTML("afterbegin", fun.forms());
+
+	const formIdentification = body[0].querySelector('.author') as HTMLFormElement;
+	const input = body[0].querySelector('input') as HTMLInputElement;
+	console.log("Получили форму для регистрации New Login!");
+
+	window.addEventListener('offline', (e: any) => {
+		console.warn("Note: User's browser id ofline now!");
+	});
+
+
+	let resultOfFormIdentification = '';
+	formIdentification.addEventListener('keypress', async (e: any) => {
+		if (((e as KeyboardEvent).key === 'Enter')) {
+			// e.preventDefault();
+			sendToServe(e);
+			input.value = '';
+		}
+
+	});
+
+	formIdentification.addEventListener('click', async (e: any) => {
+		if (((e as MouseEvent).target as HTMLButtonElement).type === 'submit') {
+			sendToServe(e);
+
+			input.value = '';
+		}
+	});
+	function sendToServe(e: any) {
+		e.preventDefault();
+
+		if (input.value.length < 1) { console.log('Длина INPUT = 0:', input.value.length); return }
+		console.log('SUBMIT'),
+			console.log('Прослушка -  получили событие Inpuut из формы New-Login');
+		resultOfFormIdentification = JSON.stringify(fun.author(e));
+		// if (resultOfFormIdentification === '{"newLogin":""}')
+		// wsSend(resultOfFormIdentification);
+		wsConnection.send(JSON.stringify(resultOfFormIdentification));
+		// function tutu() {
+
+
+
+	}
+
+	// function wsSend(elem: string) {
+
 	// function tutu() {
 	wsConnection.onopen = function () {
 		console.log("Соединение установлено.");
 		// debugger;
-		wsserver.send(JSON.stringify(resultOfFormIdentification));
-	};
 
-	wsConnection.onclose = function (event: any) {
-		if (event.wasClean) {
-			console.log('Соединение закрыто чисто');
-		} else {
-			console.log('Обрыв соединения'); // например, "убит" процесс сервера
-		}
-		console.log('Код: ' + event.code + ' причина: ' + event.reason);
 	};
+	wsConnection.addEventListener('message', function (m: any) {
+		console.log("Соединение установлено.", m.data);
+		debugger;
+
+	});
+	// wsConnection.onclose = function (event: any) {
+	// 	if (event.wasClean) {
+	// 		console.log('Соединение закрыто чисто');
+	// 	} else {
+	// 		console.log('Обрыв соединения'); // например, "убит" процесс сервера
+	// 	}
+	// 	console.log('Код: ' + event.code + ' причина: ' + event.reason);
+	// };
 
 	wsConnection.onerror = function (error: any) {
 		console.log("Ошибка " + error.message);
 	};
-}
+	// }
 
-input.value = '';
-/*------------------------------------*/
+});
 
-// });
-// wsserver.addEventListener('connection', (ws: any) => {
-// wsserver.onopen = (ctx: any) => {
-
-// 	ctx.send(JSON.stringify(resultOfFormIdentification));
-// 	console.log('Соединение WS установлено.')
-// };
-// 	ws.onmessage = (e: any) => {
-// 		console.warn('Получены WS данные:', e.data)
-
-// 		console.log('Код: ' + e.code + ' причина: ' + e.reason);
-// 	};
-// ws.onclose = (e: any) => {
-// 	if (e.wasClean) {
-// 		console.warn('Соединение WS закрыто чисто');
-// 		}
-// 		else {
-// 			console.error('Обрыв WS соединения')
-// 			console.log(e);
-// 		}
-// 	}
-// });
-
-/*---------------*/
-// const myWs = new WebSocket('ws://localhost:9000');
-// обработчик проинформирует в консоль когда соединение установится
-// myWs.onopen = function () {
-// 	console.log('подключился');
-// };
-// обработчик сообщений от сервера
-// myWs.onmessage = function (message) {
-// 	console.log('Message: %s', message.data);
-// };
-// функция для отправки echo-сообщений на сервер
-// function wsSendEcho(value) {
-// 	myWs.send(JSON.stringify({ action: 'ECHO', data: value.toString() }));
-// }
-// функция для отправки команды ping на сервер
-// function wsSendPing() {
-// 	myWs.send(JSON.stringify({ action: 'PING' }));
-// }
-// document.querySelector('button').addEventListener('click', _ => {
-// 	const v = document.querySelector('input').value
-// 	wsSendEcho(v)
-//});
-
-
-// const myWs = new WebSocket('wss://localhost:7070');
-// обработчик проинформирует в консоль когда соединение установится
-// myWs.onopen = function () {
-// 	console.log('подключился');
-// };
-// // обработчик сообщений от сервера
-// myWs.onmessage = function (message: any) {
-// 	console.log('Message: %s', message.data);
-// };
-// // функция для отправки echo-сообщений на сервер
-// function wsSendEcho(value: any) {
-// 	myWs.send(JSON.stringify({ action: 'ECHO', data: value.toString() }));
-// }
-// // функция для отправки команды ping на сервер
-// function wsSendPing() {
-// 	myWs.send(JSON.stringify({ action: 'PING' }));
-// }
-// }
-// catch (err) {
-// 	console.error('На стороне клиента ошибка:', err);
-// }
