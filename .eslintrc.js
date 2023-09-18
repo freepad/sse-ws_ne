4ce35ead6f17fb748b366371f172bbf8e3268faa
Tryd0g0lik <work80@mail.ru>
2023-09-18 14:21:35 +0700

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,18 @@ module.exports = {
 		"commonjs": true,
 		"es6": true,
 		"jest": true,
-    },
-	"extends": ["airbnb-base",
-		"standard-with-typescript"],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
 	},
-	"parser": ["babel-eslint", '@typescript-eslint/parser']
+	"extends": [
+		"airbnb-base",
+		"standard-with-typescript"
+	],
+	"overrides": [],
+	"parserOptions": {
+		"ecmaVersion": "latest"
+	},
+	"rules": {},
+	"parser": [
+		"babel-eslint",
+		"@typescript-eslint/parser"
+	]
 }
