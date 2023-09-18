3f8d8208024e59c3c4b5038bac0b12dd441af308
Tryd0g0lik <work80@mail.ru>
2023-09-18 13:53:21 +0700

    update configfile

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -14,5 +14,18 @@ module.exports = (jest, {
 		"!**__unit__/zero.test.js"
 	],
 
+	launchOptions: {
+		// Здесь можно добавить любые другие параметры, необходимые для запуска браузера с помощью puppeteer
+		// headless: true,
+		// args: ['--no-sandbox', '--disable-setuid-sandbox'],
+		pipe: true,
+		// Добавьте следующий параметр, чтобы использовать нативный WebSocket объект вместо 'ws'
+		ignoreDefaultArgs: ['--disable-extensions', '--disable-web-security'],
+	},
+	// вид страницы
+	viewport: {
+		heught: 800,
+		width: 1200,
+	}
 });
 
