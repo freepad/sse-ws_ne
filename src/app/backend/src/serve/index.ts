5515305189bede03c66860ea6af1447a47dd5410
Denis <63048913+Tryd0g0lik@users.noreply.github.com>
2023-10-27 09:51:55 +0700

    Update index.ts
    Код из 'db' перенес на 'serve'

--- src/app/backend/src/serve/index.ts
+++ src/app/backend/src/serve/index.ts
@@ -7,7 +7,7 @@ const Logger = require('koa-logger');
 const WS = require('ws');
 const { koaBody } = require('koa-body');
 const { v4 } = require('uuid');
-const db = require('./db/index.js');
+// const db = require('./db/index.js');
 const app = new Koa();
 const server = http.createServer(app.callback);
 const wss = new WS.Server({ server });
@@ -17,7 +17,31 @@ let url: string = '';
 console.log('[DB-file]: ', db);
 console.log('[DB-object]: ', Object(db))
 console.log('[DB-object]: ', Object.keys(db))
+const wss  = {
+	logins: ([] as any[]), // [ { login: 'RRRR', id: '7ef88beb-37ef-4806-9a8b-6ac6632828df' } ]
+	posts: ([] as any[]),
+	handlers: ([] as any[]),
 
+	/**
+	 *
+	 * @param item For a For a SiteEvent
+	 */
+	adds(item: any) {
+		this.logins.push(item);
+		this.handlers.forEach((handler: any) => handler(item));
+	},
+
+	/**
+	 *
+	 * @param handler For a SiteEvent
+	 */
+	listener(handler: any) {
+		this.handlers.push(handler);
+
+	}
+};
+
+// module.exports = firstDb;
 wss.on('connection', (ws: any, req: any) => {
 	ws.on('message', (m: any) => {
 		url = req.url.slice(0,);
